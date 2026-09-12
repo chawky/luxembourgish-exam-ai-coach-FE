355a9bd7c58cfaa5d86e1e4f4141af3d065aa2c9
chawky <neilychawky2@gmail.com>
2026-09-12 23:09:50 +0200

    fix IPv6 resolver address needs brackets

--- docker-entrypoint-custom.sh
+++ docker-entrypoint-custom.sh
@@ -3,6 +3,12 @@ set -e
 
 DNS_RESOLVER=$(awk '/^nameserver/ { print $2; exit }' /etc/resolv.conf)
 
+case "$DNS_RESOLVER" in
+  *:*)
+    DNS_RESOLVER="[$DNS_RESOLVER]"
+    ;;
+esac
+
 export DNS_RESOLVER
 
 exec /docker-entrypoint.sh nginx -g 'daemon off;'
